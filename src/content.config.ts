62b4e554e795d857f1f4192274a8dee3f1491e59
aimyferdy <cs.myferdy@gmail.com>
2026-07-28 12:49:11 +0700

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,16 +14,25 @@ const aplikasi = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+
     pubDate: z.coerce.date(),
+
     image: z.string().optional(),
+    hero: z.string().optional(),
+    icon: z.string().optional(),
+
     popular: z.boolean().optional(),
 
     category: z.string().optional(),
     version: z.string().optional(),
     size: z.string().optional(),
+
     rating: z.number().optional(),
     downloads: z.string().optional(),
+
     android: z.string().optional(),
+    updated: z.string().optional(),
+
     download: z.string().optional(),
   }),
 });
