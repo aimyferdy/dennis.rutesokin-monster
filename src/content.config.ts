87a0292d68c0be0ba099395a039bc019a1e57370
aimyferdy <cs.myferdy@gmail.com>
2026-07-28 11:56:43 +0700

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,31 @@
 import { defineCollection, z } from "astro:content";
 
-const schema = z.object({
-  title: z.string(),
-  description: z.string(),
-  pubDate: z.coerce.date(),
-  updatedDate: z.coerce.date().optional(),
-  image: z.string().optional(),
-  popular: z.boolean().optional(),
-
-  category: z.string().optional(),
-  version: z.string().optional(),
-  size: z.string().optional(),
-  rating: z.number().optional(),
-});
-
 const blog = defineCollection({
-  schema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    image: z.string().optional(),
+    popular: z.boolean().optional(),
+  }),
 });
 
 const aplikasi = defineCollection({
-  schema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    image: z.string().optional(),
+    popular: z.boolean().optional(),
+
+    category: z.string().optional(),
+    version: z.string().optional(),
+    size: z.string().optional(),
+    rating: z.number().optional(),
+    downloads: z.string().optional(),
+    android: z.string().optional(),
+    download: z.string().optional(),
+  }),
 });
 
 export const collections = {
